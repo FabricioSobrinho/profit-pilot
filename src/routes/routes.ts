d1f3b3cf8a7e8bc49c42fa33604ebda389a2bb4f
FabricioSobrinho <fabriciomails3@gmail.com>
2024-03-26 15:26:12 -0400

    Feat: add new routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { BarracaController } from "../controllers/BarracaController";
 import { AuthController } from "../controllers/AuthController";
+import { VendasController } from "../controllers/VendasController";
 
 import { authMiddleware } from "../middlewares/auth";
 
@@ -8,9 +9,15 @@ export const router = Router();
 
 const barracaController = new BarracaController();
 const authController = new AuthController();
+const vendasController = new VendasController();
 
+router.get("/barracas", barracaController.index);
 router.post("/barracas", barracaController.create);
+
 router.post("/login", authController.login);
 
-router.use(authMiddleware)
-router.get("/barracas", barracaController.index);
+router.use(authMiddleware);
+router.get("/barraca", barracaController.getBarraca);
+
+router.get("/vendas",  vendasController.Index);
+router.post("/vendas",  vendasController.novaVenda);
