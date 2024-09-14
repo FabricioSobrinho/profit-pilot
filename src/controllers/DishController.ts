0a71b93bacd302ded7fbd848db32e5cba6deb4a9
FabricioSobrinho <fabriciomails3@gmail.com>
2024-09-14 19:11:17 -0400

    Feat: insert CRUD controllers

--- src/controllers/DishController.ts
+++ src/controllers/DishController.ts
@@ -2,6 +2,16 @@ import { Request, Response } from "express";
 import { prisma } from "../utils/prisma";
 
 export class DishController {
+  async getDishs(req: Request, res: Response) {
+    try {
+      const dishs = await prisma.dish.findMany();
+
+      res.status(200).json(dishs);
+    } catch (e: any) {
+      res.status(500).json({ error: e.message });
+    }
+  }
+
   async newDish(req: Request, res: Response) {
     try {
       const tentId = req.tentId;
@@ -24,6 +34,42 @@ export class DishController {
     } catch (error) {
       console.error("Erro ao criar prato:", error);
       return res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+  async updateDish(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const { name, value } = req.body;
+      const tentId = req.tentId;
+
+      const updatedDish = await prisma.dish.update({
+        where: { id: Number(id) },
+        data: {
+          name,
+          value,
+          tentId,
+        },
+      });
+
+      res.status(200).json(updatedDish);
+    } catch (e: any) {
+      console.error("Erro ao atualizar o prato:", e);
+      res.status(500).json({ error: e.message });
+    }
+  }
+
+  async deleteDish(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+
+      const deleteDish = await prisma.dish.delete({
+        where: { id: Number(id) },
+      });
+
+      res.status(200).json({message: "Deleted dish"});
+    } catch (e: any) {
+      console.error("Erro ao deletar o prato:", e);
+      res.status(500).json({ error: e.message });
     }
   }
 }
