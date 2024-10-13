b03587b21d0d08499f808a328c6799e2cee8330b
FabricioSobrinho <fabriciomails3@gmail.com>
2024-10-13 19:43:21 -0400

    Feat: create cors config

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,25 @@
 import express from "express";
 import { router } from "./routes/routes";
+import cors from "cors";
 
 const app = express();
 
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, 
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
-app.use(router)
+app.use(router);
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
-  console.log(`Servidor operando 🚀!\nEndereço do servidor: http://localhost:${port}`)
+  console.log(
+    `Servidor operando 🚀!\nEndereço do servidor: http://localhost:${port}`
+  )
 );
