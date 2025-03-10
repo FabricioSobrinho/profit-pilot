569cd92d9d8b59a93e926be178875f75e62545f8
FabricioSobrinho <fabriciomails3@gmail.com>
2025-03-09 21:01:54 -0400

    Feat: add cors allowed origins

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express();
 
 const corsOptions = {
-  origin: "https://milho-pay.onrender.com",
+  origin: process.env.CORS_REQUEST_ALLOWED,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, 
